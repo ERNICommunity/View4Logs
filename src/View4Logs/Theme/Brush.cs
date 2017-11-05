0d6ef5c610b2d359804c4fca2bbbd5bd7e488fcc
Michal Dorner <dorner.michal@gmail.com>
2017-11-05 18:22:41 +0100

    Concept  of theming

---
+++ src/View4Logs/Theme/Brush.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace View4Logs.Theme
+{
+    public static class Brush
+    {
+        public static readonly ComponentResourceKey Background = new ComponentResourceKey(typeof(Brush), nameof(Background));
+        public static readonly ComponentResourceKey Border = new ComponentResourceKey(typeof(Brush), nameof(Border));
+        public static readonly ComponentResourceKey Text = new ComponentResourceKey(typeof(Brush), nameof(Text));
+    }
+}
