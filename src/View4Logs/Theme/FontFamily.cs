a4c98c4014ced929339acec6f0bd2f05e47abeeb
Michal Dorner <dorner.michal@gmail.com>
2017-11-05 19:39:41 +0100

    Concept of typography

---
+++ src/View4Logs/Theme/FontFamily.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace View4Logs.Theme
+{
+    public static class FontFamily
+    {
+        public static readonly ComponentResourceKey Default = new ComponentResourceKey(typeof(FontFamily), nameof(Default));
+    }
+}
---
+++ src/View4Logs/Theme/FontSize.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace View4Logs.Theme
+{
+    public static class FontSize
+    {
+        public static readonly ComponentResourceKey ExtraSmall = new ComponentResourceKey(typeof(FontSize), nameof(ExtraSmall));
+        public static readonly ComponentResourceKey Small = new ComponentResourceKey(typeof(FontSize), nameof(Small));
+        public static readonly ComponentResourceKey Normal = new ComponentResourceKey(typeof(FontSize), nameof(Normal));
+        public static readonly ComponentResourceKey Large = new ComponentResourceKey(typeof(FontSize), nameof(Large));
+        public static readonly ComponentResourceKey ExtraLarge = new ComponentResourceKey(typeof(FontSize), nameof(ExtraLarge));
+    }
+}
