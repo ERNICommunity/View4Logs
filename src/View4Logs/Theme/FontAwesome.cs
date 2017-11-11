30621caff102bc571d844c1bb01fa7f0f3cb6a53
Michal Dorner <dorner.michal@gmail.com>
2017-11-11 21:17:40 +0100

    Add icon font - FontAwesome

---
+++ src/View4Logs/Theme/FontAwesome.cs
@@ -0,0 +1,7 @@
+﻿namespace View4Logs.Theme
+{
+    public static class FontAwesome
+    {
+        public const string Database = "\uF1C0";
+    }
+}
--- src/View4Logs/Theme/FontFamily.cs
+++ src/View4Logs/Theme/FontFamily.cs
@@ -5,5 +5,6 @@ namespace View4Logs.Theme
     public static class FontFamily
     {
         public static readonly ComponentResourceKey Default = new ComponentResourceKey(typeof(FontFamily), nameof(Default));
+        public static readonly ComponentResourceKey FontAwesome = new ComponentResourceKey(typeof(FontFamily), nameof(FontAwesome));
     }
 }
