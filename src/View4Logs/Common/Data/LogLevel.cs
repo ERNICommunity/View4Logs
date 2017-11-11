7b750d511c02578002a06fd640c359afc69aa166
Michal Dorner <dorner.michal@gmail.com>
2017-11-11 10:27:32 +0100

    Add LogLevel + extend LogMessage

---
+++ src/View4Logs/Common/Data/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace View4Logs.Common.Data
+{
+    public enum LogLevel : byte
+    {
+        All,
+        Trace,
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal,
+        Off = byte.MaxValue,
+    }
+}
--- src/View4Logs/Common/Data/LogMessage.cs
+++ src/View4Logs/Common/Data/LogMessage.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace View4Logs.Common.Data
 {
     public class LogMessage
     {
         public string Message { get; set; }
+
+        public LogLevel Level { get; set; }
+
+        public string LoggerName { get; set; }
+
+        public DateTime TimeStamp { get; set; }
     }
 }
