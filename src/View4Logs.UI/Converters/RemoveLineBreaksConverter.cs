232e41ddffe9fa59ec72ec16d6ee4303a4a03ba0
Michal Dorner <dorner.michal@gmail.com>
2017-12-09 00:17:40 +0100

    Handle line breaks and long text in LogsView

---
+++ src/View4Logs.UI/Converters/RemoveLineBreaksConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace View4Logs.UI.Converters
+{
+    public sealed class RemoveLineBreaksConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string)?.Replace('\n', ' ');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
